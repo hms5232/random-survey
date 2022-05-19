1d598f8873abffe43bc468e8bcab337dc67fd378
timmylinlin <78272252+timmy0801@users.noreply.github.com>
2022-05-19 15:06:13 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,9 +8,7 @@
 
 
 const urls = [
-	'https://www.surveycake.com/s/kYmP4',
 	'https://www.surveycake.com/s/q3BON',
-	'https://www.surveycake.com/s/kYDW3',
 	'https://www.surveycake.com/s/XbOv8',
 	'https://www.surveycake.com/s/214nV',
 	'https://www.surveycake.com/s/V9loM',
