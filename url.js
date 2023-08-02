9d03cdfd4b61f771303b151b5774a539684c0a9b
BoBoY05 <131180708+BoBoY05@users.noreply.github.com>
2023-08-02 20:20:47 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,16 +8,10 @@
 
 
 const urls = [
-	'https://www.surveycake.com/s/KOa8N',
-	'https://www.surveycake.com/s/WlWZB',
-	'https://www.surveycake.com/s/YlaX7',
-	'https://www.surveycake.com/s/y82Qp',
-	'https://www.surveycake.com/s/xxXyD',
-	'https://www.surveycake.com/s/NLdaN',
-	'https://www.surveycake.com/s/44Ok6',
-	'https://www.surveycake.com/s/AwVBB',
-	'https://www.surveycake.com/s/z7mAM',
+	'https://www.surveycake.com/s/9Q1nP',
+	'https://www.surveycake.com/s/bxP99',
+	'https://www.surveycake.com/s/Q3YpR',
+	'https://www.surveycake.com/s/4bVRQ',
 	'https://www.surveycake.com/s/ml9Wx',
-	'https://www.surveycake.com/s/baN6g',
-	'https://www.surveycake.com/s/QQa4y',
+	'https://www.surveycake.com/s/xxXyD',
 ];
