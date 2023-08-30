079999c04e56d62c1bb53ef1e2a6a6d72cb0db0e
Arthurli24 <143592487+Arthurli24@users.noreply.github.com>
2023-08-31 02:06:41 +0800

    delicacy url.js

--- url.js
+++ url.js
@@ -8,7 +8,12 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232'
+	'https://docs.google.com/forms/d/15R5p7tvhMcRmaDMaYst2TY2FBCSUhDAEm4CybBzQTFQ/edit',
+	'https://docs.google.com/forms/d/1R1Qosrphoku8VbG6IbBJjBvT1X03brKr-L-jSTtYB1Y/edit',
+	'https://docs.google.com/forms/d/1_Fx2-Me2fUVPBWS2uyLiWjLu_sjKTM5rDcynl7FSNFc/edit',
+	'https://docs.google.com/forms/d/1IOy3xIXSD2omh2fShUuX95brcaFBNUggaziLD6z9F3M/edit',
+	'https://docs.google.com/forms/d/1KecVLtMVqksR305inEWGrOnsLUIGZFHvesFpZ97K4qo/edit',
+	'https://docs.google.com/forms/d/1_BljVfafgM3UzRAfeeO6iLNzQjQDP0aHigOivi0vg3s/edit',
+	'https://docs.google.com/forms/d/1_sH-aUfFU2hZT8LuosDuo0j0I3tJ24l126pW221Xuow/edit',
+	'https://docs.google.com/forms/d/1So8acexGaZX_iD-MNcKrI4NEZrNTWLyeHCawqG4WlkY/edit'
 ];
