2d443496535b5393a71c99211b247060f51b66cd
LEEYUHSNU <leemichael2002@gmail.com>
2024-05-22 12:18:03 +0800

    顧客忠誠計畫 url.js

--- url.js
+++ url.js
@@ -8,7 +8,6 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232'
+	'https://forms.gle/AfgsBoj54KfXWs8o6,
+	'https://forms.gle/BqkAtWTdWqnzMQ5XA,
 ];
